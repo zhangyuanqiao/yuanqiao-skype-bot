89bd489fa56969bec50b2464bd7e50287f81edda
yuanqiao Zhang <yuanqiao.zhang@mindgeek.com>
2016-08-31 12:32:18 -0400

    init

---
+++ app.js
@@ -0,0 +1,133 @@
+/*-----------------------------------------------------------------------------
+This Bot uses the Bot Connector Service but is designed to showcase whats 
+possible on Skype using the framework. The demo shows how to create a looping 
+menu, use the built-in prompts, send Pictures, send Hero & Thumbnail Cards, 
+send Receipts, and use Carousels. 
+
+# RUN THE BOT:
+
+    You can run the bot locally using the Bot Framework Emulator but for the best
+    experience you should register a new bot on Skype and bind it to the demo 
+    bot. You can then run the bot locally using ngrok found at https://ngrok.com/.
+
+    * Install and run ngrok in a console window using "ngrok http 3978".
+    * Create a bot on https://dev.botframework.com and follow the steps to setup
+      a Skype channel.
+    * For the endpoint you setup on dev.botframework.com, copy the https link 
+      ngrok setup and set "<ngrok link>/api/messages" as your bots endpoint.
+    * Next you need to configure your bots MICROSOFT_APP_ID, and
+      MICROSOFT_APP_PASSWORD environment variables. If you're running VSCode you 
+      can add these variables to your the bots launch.json file. If you're not 
+      using VSCode you'll need to setup these variables in a console window.
+      - MICROSOFT_APP_ID: This is the App ID assigned when you created your bot.
+      - MICROSOFT_APP_PASSWORD: This was also assigned when you created your bot.
+    * To use the bot you'll need to click the join link in the portal which will
+      add it as a contact to your skype account. 
+    * To run the bot you can launch it from VSCode or run "node app.js" from a 
+      console window. 
+
+-----------------------------------------------------------------------------*/
+
+var restify = require('restify');
+var builder = require('./core');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+
+ /*   appId: 'b418b21c-5e1a-4538-9551-23c3be43e636',
+    appPassword: 'RY2WibcAZsGVtcDe7OVxJXk'*/
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+
+//=========================================================
+// Activity Events
+//=========================================================
+
+bot.on('conversationUpdate', function (message) {
+   // Check for group conversations
+    message.membersAdded.forEach(function (identity) {
+        if (identity.id === message.address.bot.id) {
+            var reply = new builder.Message()
+                .address(message.address)
+                .text("Hello everyone!");
+            bot.send(reply);
+        }
+    });
+});
+
+bot.on('contactRelationUpdate', function (message) {
+    if (message.action === 'add') {
+        var name = message.user ? message.user.name : null;
+        var reply = new builder.Message()
+                .address(message.address)
+                .text("Hello %s... Thanks for adding me. Say 'hello' to see some great demos.", name || 'there');
+        bot.send(reply);
+    } else {
+        // delete their data
+    }
+});
+
+bot.on('deleteUserData', function (message) {
+    // User asked to delete their data
+});
+
+
+//=========================================================
+// Bots Middleware
+//=========================================================
+
+// Anytime the major version is incremented any existing conversations will be restarted.
+bot.use(builder.Middleware.dialogVersion({ version: 1.0, resetCommand: /^reset/i }));
+
+//=========================================================
+// Bots Global Actions
+//=========================================================
+
+bot.endConversationAction('goodbye', 'Goodbye :)', { matches: /^goodbye/i });
+bot.beginDialogAction('help', '/help', { matches: /^help/i });
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', [
+    function (session) {
+
+        var msg1=session.message.text;
+        session.send("hello baby:  "+msg1);
+        // Send a greeting and show help.
+        var card = new builder.HeroCard(session)
+            .title("Microsoft Bot Framework")
+            .text("Your bots - wherever your users are talking.")
+            .images([
+                 builder.CardImage.create(session, "http://docs.botframework.com/images/demo_bot_image.png")
+            ]);
+        var msg = new builder.Message(session).attachments([card]);
+        session.send(msg);
+        session.send("Hi... I'm the Microsoft Bot Framework demo bot for Skype. I can show you everything you can use our Bot Builder SDK to do on Skype.");
+        //session.beginDialog('/help');
+    },
+    function (session, results) {
+        // Display menu
+        session.beginDialog('/menu');
+    },
+    function (session, results) {
+        // Always say goodbye
+        session.send("Ok... See you later!");
+    }
+]);
+
